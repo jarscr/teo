c76abc295f950ab6cf6eba9f6a6968eaa5ce6c8e
Alfredo Rodriguez <jarscr@gmail.com>
2020-12-29 15:29:51 -0600

    first commit

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Front controller
+ *
+ * PHP version 7.0
+ */
+
+/**
+ * Composer
+ */
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+
+/**
+ * Error and Exception handling
+ */
+error_reporting(E_ALL);
+set_error_handler('Core\Error::errorHandler');
+set_exception_handler('Core\Error::exceptionHandler');
+
+
+/**
+ * Routing
+ */
+$router = new Core\Router();
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('{controller}/{action}');
+$router->add('login', ['controller' => 'Login', 'action' => 'index']);
+$router->dispatch($_SERVER['QUERY_STRING']);
