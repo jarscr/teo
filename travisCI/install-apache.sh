c400938042e178c85a3cd7b9895f02ba092dedff
Alfredo Rodriguez <jarscr@gmail.com>
2021-01-26 17:14:44 -0600

    Travis-CI Update

--- travisCI/install-apache.sh
+++ travisCI/install-apache.sh
@@ -5,11 +5,10 @@
 # Install Apache2
 sudo apt-get update
 sudo apt-get install -y --force-yes apache2
-# Add PHP 7.0 Repository
-sudo add-apt-repository -y ppa:ondrej/php
-# Install PHP 7.0
-sudo apt-get update
-sudo apt-get install -y --force-yes php7.3 libapache2-mod-php7.3 php7.3-fpm
+# Add PHP 7.3 Repository
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
+sudo apt-get install -y --force-yes php7.3 php7.3-common php7.3-mysql php7.3-xml php7.3-xmlrpc php7.3-curl php7.3-gd php7.3-imagick php7.3-cli php7.3-dev php7.3-imap php7.3-mbstring php7.3-opcache php7.3-soap php7.3-zip php7.3-intl -y
 # Copy our virtual host template to sites-enabled overwriting the default site conf
 sudo cp travisCI/defaultsite.tpl /etc/apache2/sites-available/000-default.conf
 # Copy basic testing files into /var/www
